e6f713b69720d97859b74fd6f9c18020d5a241e5
jasonf <jasonf@pop-os.localdomain>
2020-09-13 17:37:01 -0500

    Added theme module to handle theme toggle

---
+++ src/app/theme/light-theme.ts
@@ -0,0 +1,22 @@
+import { Theme } from './symbols';
+
+export const lightTheme: Theme = {
+  name: 'light',
+  properties: {
+    '--primary': '#87f7ff',
+    '--primary-text': '#132c2e',
+    '--primary-alt': '#ffc400',
+    '--secondary': '#ce93d8',
+    '--secondary-text': '#2a1a2b',
+    '--secondary-alt': '#b7e391',
+    '--accent': '#ff1744',
+    '--nav': '#1e359c',
+    '--nav-text': '#dbdbdb',
+    '--background-color': '#fafafa',
+    '--image-filter': 'grayscale(0%)',
+    '--line-one-text': '#005799',
+    '--line-two-text': '#44276b',
+    '--line-three-text': '#3e7310',
+    '--line-four-text': '#ad4311',
+  },
+};
