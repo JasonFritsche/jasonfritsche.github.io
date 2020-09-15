9ce0fc75c556cb70b0a2d8b01107f77295995d5a
jasonf <jasonf@pop-os.localdomain>
2020-09-14 19:22:19 -0500

    Updated mat-menu theme style

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ThemeService } from 'src/app/theme/theme.service';
+import { Theme } from 'src/app/theme/symbols';
 
 @Component({
   selector: 'app-navbar',
@@ -10,16 +11,32 @@ export class NavbarComponent implements OnInit {
   @Input('class') panelClass: string;
   public themeIcon: string = 'white-balance-sunny';
   public title: string = '<Jason/>';
+  public currentTheme: string;
+  public menuPanelBackgroundColor: string;
+  public menuPanelTextColor: string;
 
   constructor(private themeService: ThemeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const theme = this._getTheme();
+    this.currentTheme = theme.name === 'dark' ? 'dark' : 'light';
+    this._updateNavbar(this.currentTheme);
+  }
 
   public changeTheme(): void {
-    const theme = this.themeService.getActiveTheme();
-    const newTheme = theme.name === 'dark' ? 'light' : 'dark';
-    this.themeIcon =
-      newTheme === 'dark' ? 'weather-night' : 'white-balance-sunny';
-    this.themeService.setTheme(newTheme);
+    const theme = this._getTheme();
+    this.currentTheme = theme.name === 'dark' ? 'light' : 'dark';
+    this._updateNavbar(this.currentTheme);
+    this.themeService.setTheme(this.currentTheme);
+  }
+
+  private _getTheme(): Theme {
+    return this.themeService.getActiveTheme();
+  }
+
+  private _updateNavbar(theme: string) {
+    this.themeIcon = theme === 'dark' ? 'weather-night' : 'white-balance-sunny';
+    this.menuPanelBackgroundColor = theme === 'dark' ? '#292c2e' : '#1e359c';
+    this.menuPanelTextColor = theme === 'dark' ? '#d1d1d1' : '#dbdbdb';
   }
 }
